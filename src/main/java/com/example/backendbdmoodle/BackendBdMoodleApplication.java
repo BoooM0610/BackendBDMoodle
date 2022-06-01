2688dab2faccc4a78f5fea786b00ba7d4a4b1302
BoooM0610 <eduardomartinezoc@gmail.com>
2022-06-01 10:43:48 +0200

    Initial commit

---
+++ src/main/java/com/example/backendbdmoodle/BackendBdMoodleApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendbdmoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBdMoodleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBdMoodleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendbdmoodle/BackendBdMoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendbdmoodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBdMoodleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
